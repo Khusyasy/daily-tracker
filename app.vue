f99a3e415864f2941196b3a796d0341b282229a5
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2025-07-11 02:39:23 +0700

    feat: add delete mode and uncheckin task

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <script setup>
-  import { dateFromNow, dateToNow } from './utils/date'
-
   useHead({
     title: 'My Vue App',
     meta: [
@@ -15,6 +13,7 @@
     url: '',
     refreshTime: '',
   })
+  const deleteMode = ref(false)
   const clockTime = ref(new Date())
 
   function handleSubmit() {
@@ -44,6 +43,9 @@
 
   function checkinTask(id) {
     const task = tasks.value.find(task => task.id === id)
+    if (task.url) {
+      window.open(task.url, '_blank')
+    }
     if (task) {
       const now = new Date()
       task.lastCheckin = now
@@ -55,7 +57,15 @@
     }
   }
 
-  // handle clock update every second
+  function uncheckinTask(id) {
+    const task = tasks.value.find(task => task.id === id)
+    if (task) {
+      task.lastCheckin = null
+      task.done = false
+      checkins.value = checkins.value.filter(checkin => checkin.taskId !== id)
+    }
+  }
+
   setInterval(() => {
     clockTime.value = new Date()
   }, 100)
@@ -92,8 +102,8 @@
 
       <div>
         <button @click.prevent="handleSubmit" type="button"
-          class="flex items-center justify-center w-12 h-12 text-sm font-semibold text-white bg-green-600 rounded-full hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
-          <Icon name="mdi:plus" class="w-4 h-4" />
+          class="flex items-center justify-center w-12 h-12 text-sm font-semibold text-white bg-green-600 rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+          <Icon name="mdi:plus" class="w-5 h-5" />
         </button>
       </div>
     </div>
@@ -106,39 +116,53 @@
         <p class="text-lg font-bold text-gray-800">
           {{ timeFormat(clockTime) }}
         </p>
+        <button @click="deleteMode = !deleteMode"
+          class="ml-2 p-2 flex items-center justify-center text-sm font-medium rounded" :class="{
+            'text-red-600 hover:text-red-700 bg-red-50 hover:bg-red-100': !deleteMode,
+            'text-red-50 hover:text-red-100 bg-red-600 hover:bg-red-700': deleteMode
+          }">
+          <Icon name="mdi:delete" class="w-5 h-5" />
+        </button>
       </div>
       <ul class="mt-2 space-y-2">
-        <li v-for="task in tasks" :key="task.id" class="bg-white rounded shadow">
-          <a class="flex items-center justify-between p-4 gap-4" :href="task.url" target="_blank"
-            rel="noopener noreferrer" @click="task.done ? null : checkinTask(task.id)">
+        <!-- TODO: drag and drop reorder task thingy -->
+        <li v-for="task in tasks" :key="task.id" class="bg-white hover:bg-gray-50 rounded shadow">
+          <div class="flex items-center justify-between p-4 gap-4">
             <div class="flex-1 flex flex-row items-center justify-between">
               <h3 class="text-lg font-medium text-gray-900">
                 {{ task.task }}
               </h3>
               <div class="flex flex-row gap-2">
                 <p v-if="task.done" class="text-md text-green-600 flex items-center justify-center">
-                  <Icon name="mdi:check" class="inline-block w-5 h-5 mr-1" />
+                  <Icon name="mdi:check" class="w-5 h-5 mr-1" />
                   {{ dateFromNow(task.lastCheckin) }}
                 </p>
                 <p class="text-md text-gray-600 flex items-center justify-center">
-                  <Icon name="mdi:refresh" class="inline-block w-5 h-5 mr-1" />
+                  <Icon name="mdi:refresh" class="w-5 h-5 mr-1" />
                   {{ task.refreshTime }}
                 </p>
               </div>
             </div>
-            <div>
-              <button @click.prevent="task.done ? null : checkinTask(task.id)"
-                class="text-green-600 hover:text-green-800 px-4 py-2 rounded-s bg-green-50 hover:bg-green-100"
+            <div v-if="!deleteMode">
+              <button @click.stop.prevent="task.done ? null : checkinTask(task.id)"
+                class="flex items-center justify-center p-2 rounded text-green-600 hover:text-green-800 bg-green-100 hover:bg-green-200"
                 :class="{ 'opacity-50 cursor-not-allowed': task.done }">
                 <Icon v-if="task.done" name="mdi:check-bold" class="w-5 h-5" />
                 <Icon v-else name="mdi:send" class="w-5 h-5" />
               </button>
-              <button @click.prevent="removeTask(task.id)"
-                class="text-red-600 hover:text-red-800 px-4 py-2 rounded-e bg-red-50 hover:bg-red-100">
+            </div>
+            <div v-if="deleteMode" class="flex flex-row items-center">
+              <button @click.stop.prevent="task.done ? uncheckinTask(task.id) : null"
+                class="flex items-center justify-center p-2 rounded-s text-gray-600 hover:text-gray-800 bg-gray-100 hover:bg-gray-200"
+                :class="{ 'opacity-50 cursor-not-allowed': !task.done }">
+                <Icon name="mdi:close" class="w-5 h-5" />
+              </button>
+              <button @click.stop.prevent="removeTask(task.id)"
+                class="flex items-center justify-center p-2 rounded-e text-red-600 hover:text-red-800 bg-red-100 hover:bg-red-200">
                 <Icon name="mdi:delete" class="w-5 h-5" />
               </button>
             </div>
-          </a>
+          </div>
         </li>
         <li v-if="tasks.length === 0" class="text-gray-500 text-center p-4">
           No tasks added yet.
