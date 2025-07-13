f23874112057412a3fbdfb00fa59b9503637ec3f
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2025-07-13 13:11:11 +0700

    fix: realign ui

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@
     url: '',
     refreshTime: '',
   })
-  const deleteMode = ref(false)
+  const editMode = ref(false)
   const clockTime = ref(new Date())
 
   const tasksDone = computed(() => {
@@ -150,7 +150,7 @@
     <Header />
 
     <form @submit.prevent="handleSubmit"
-      class="flex flex-row items-center justify-center w-full max-w-4xl my-4 mx-auto gap-2">
+      class="flex flex-row items-end justify-center w-full max-w-4xl my-4 mx-auto px-4 gap-2">
       <div class="flex-1">
         <label for="task" class="block text-sm font-medium text-gray-700">
           Task
@@ -167,7 +167,7 @@
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" />
       </div>
 
-      <div class="flex-1">
+      <div class="flex-[0.75]">
         <label for="refresh-time" class="block text-sm font-medium text-gray-700">
           Refresh Time (local time)
         </label>
@@ -177,26 +177,26 @@
 
       <div>
         <button type="submit" aria-label="Add task"
-          class="flex items-center justify-center w-12 h-12 text-sm font-semibold text-white bg-green-600 rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+          class="flex items-center justify-center p-2 text-sm font-semibold text-white bg-green-600 rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
           <Icon name="mdi:plus" class="w-5 h-5" />
         </button>
       </div>
     </form>
 
     <div class="max-w-4xl mx-auto my-4">
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between px-4">
         <h2 class="text-lg font-semibold text-gray-800">
           Tasks
         </h2>
         <p class="text-lg font-bold text-gray-800">
           {{ timeFormat(clockTime) }}
         </p>
-        <button @click="deleteMode = !deleteMode" aria-label="Toggle delete mode"
+        <button @click="editMode = !editMode" aria-label="Toggle edit mode"
           class="ml-2 p-2 flex items-center justify-center text-sm font-medium rounded" :class="{
-            'text-red-600 hover:text-red-700 bg-red-50 hover:bg-red-100': !deleteMode,
-            'text-red-50 hover:text-red-100 bg-red-600 hover:bg-red-700': deleteMode
+            'text-red-600 hover:text-red-700 bg-red-50 hover:bg-red-100': !editMode,
+            'text-red-50 hover:text-red-100 bg-red-600 hover:bg-red-700': editMode
           }">
-          <Icon name="mdi:delete" class="w-5 h-5" />
+          <Icon name="mdi:note-edit" class="w-5 h-5" />
         </button>
       </div>
       <ul class="mt-2 space-y-2">
@@ -230,7 +230,7 @@
                 </p>
               </div>
             </div>
-            <div v-if="!deleteMode">
+            <div v-if="!editMode">
               <button @click.stop.prevent="checkinTask(task.id)" :aria-label="task.url ? 'Open URL' : 'Check-in task'"
                 class="flex items-center justify-center p-2 rounded text-green-600 hover:text-green-800 bg-green-100 hover:bg-green-200">
                 <Icon v-if="tasksDone[task.id]" name="mdi:check-bold" class="w-5 h-5" />
@@ -238,13 +238,13 @@
                 <Icon v-else name="mdi:send-outline" class="w-5 h-5" />
               </button>
             </div>
-            <div v-if="deleteMode" class="flex flex-row items-center">
+            <div v-if="editMode" class="flex flex-row items-center">
               <button @click.stop.prevent="tasksDone[task.id] ? uncheckinTask(task.id) : null"
                 aria-label="Uncheck-in task" :disabled="!tasksDone[task.id]"
                 class="flex items-center justify-center p-2 rounded-s text-gray-600 hover:text-gray-800 bg-gray-100 hover:bg-gray-200">
                 <Icon name="mdi:close" class="w-5 h-5" />
               </button>
-              <button @click.stop.prevent="removeTask(task.id)" aria-label="Delete task" :disabled="!deleteMode"
+              <button @click.stop.prevent="removeTask(task.id)" aria-label="Delete task" :disabled="!editMode"
                 class="flex items-center justify-center p-2 rounded-e text-red-600 hover:text-red-800 bg-red-100 hover:bg-red-200">
                 <Icon name="mdi:delete" class="w-5 h-5" />
               </button>
